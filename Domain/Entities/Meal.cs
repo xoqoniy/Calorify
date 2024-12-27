44fea783d4dfe48eb6abf71e1148713be5bc2ddb
xoqoniy <sm.ashurov7@gmail.com>
2024-12-27 17:37:45 +0500

    Entities are added

---
+++ Domain/Entities/Meal.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Audibles;
+using Domain.Enums;
+
+namespace Domain.Entities;
+
+public class Meal : Audible
+{
+    public string UserId { get; set; } // Foreign key to User table
+    public MealType MealType { get; set; } // (breakfast, lunch, dinner, snack)
+    public string PhotoUrl { get; set; } // If the user uploaded a meal photo
+    public string FoodItems { get; set; } // JSON format - list of food items
+    public ushort TotalCalories { get; set; }
+    public float TotalProtein { get; set; }
+    public float TotalCarbs { get; set; }
+    public float TotalFat { get; set; }
+}
